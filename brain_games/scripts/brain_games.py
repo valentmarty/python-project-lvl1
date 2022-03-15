c959592b59e17611f254eb0a382f989355b5caeb
valentmarty <valentmarty2018@gmail.com>
2022-03-15 19:53:04 +0300

    step 3: import welcome_user

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
 
 def main():
-    print('Welcome to the Brain Games!')
+    print('Welcome to the BRAIN Games!')
+    from brain_games.cli import  welcome_user
+    welcome_user()
+
 if __name__ == '__main__':
     main()
+
+
+
+
+
+
 
 
