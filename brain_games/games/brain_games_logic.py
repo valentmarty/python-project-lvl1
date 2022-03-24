cc3165e843f74089544e4f32b03e6cafc89000ee
valentmarty <valentmarty2018@gmail.com>
2022-03-24 19:11:31 +0300

    step 6 part 1.4: brain-even works with brain_games_logic.py

--- brain_games/games/brain_games_logic.py
+++ brain_games/games/brain_games_logic.py
@@ -1,4 +1,8 @@
 import prompt
+import random
+
+
+ROUNDS_MAX_NUMBER = 3
 
 
 def welcome():
@@ -6,8 +10,27 @@ def welcome():
     name = prompt.string('May I have your name? ')
     print('Hello, ' + name)
     return(name)
+
+
 def game_condition():
     print('Answer "yes" if the number is even, otherwise answer "no".')
+
+
+def game_body():
+    random_number = random.randint(1, 100)
+    print('Question: ', random_number)
+    right_answer = random_number % 2
+    mirror_answer = right_answer
+    answer = prompt.string('Your answer: ')
+    if answer == 'yes':
+        answer_for_comparing = 0
+        mirror_answer = 'no'
+    elif answer == 'no':
+        answer_for_comparing = 1
+        mirror_answer = 'yes'
+    return(answer, right_answer, answer_for_comparing, mirror_answer)
+
+
 def answer_check(answer, right_answer, answer_for_comparing, mirror_answer, i):
     if right_answer == answer_for_comparing:
         print('Correct!')
@@ -16,15 +39,10 @@ def answer_check(answer, right_answer, answer_for_comparing, mirror_answer, i):
         print("'" + answer + "' is wrong answer ;(. Correct answer was '" + mirror_answer + "'.")
         i = 404
     return(i)
+
+
 def rounds_check(i, name):
     if i == 3:
         print('Congratulations, ' + name + '!')
     else:
         print("Let's try again, " + name + '!')
-
-
-
-
-
-
-
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,26 +1,14 @@
 #!/usr/bin/env python
 
-import prompt
-import random
-from brain_games.games.brain_games_logic import welcome, game_condition, answer_check, rounds_check
+from brain_games.games.brain_games_logic import welcome, game_condition, game_body, answer_check, rounds_check, ROUNDS_MAX_NUMBER
 
 
 def main():
     name = welcome()
     game_condition()
     i = 0
-    while i <= 2:
-        random_number = random.randint(1, 100)
-        print('Question: ', random_number)
-        right_answer = random_number % 2
-        mirror_answer = right_answer
-        answer = prompt.string('Your answer: ')
-        if answer == 'yes':
-            answer_for_comparing = 0
-            mirror_answer = 'no'
-        elif answer == 'no':
-            answer_for_comparing = 1
-            mirror_answer = 'yes'
+    while i <= (ROUNDS_MAX_NUMBER - 1):
+        (answer, right_answer, answer_for_comparing, mirror_answer) = game_body()
         i = answer_check(answer, right_answer, answer_for_comparing, mirror_answer, i)
     # Check program
     rounds_check(i, name)
