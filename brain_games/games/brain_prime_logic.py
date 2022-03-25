aef16e90263117c94ebf08f12a821ef77c6177fe
valentmarty <valentmarty2018@gmail.com>
2022-03-25 13:16:42 +0300

    step 9.2: some changes for project and flake8 requirements

--- brain_games/games/brain_prime_logic.py
+++ brain_games/games/brain_prime_logic.py
@@ -6,7 +6,7 @@ def game_condition():
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
 
 
-def game_body():
+def game_body_1():
     random_number = random.randint(1, 100)
     print('Question:', random_number)
     i = 2
@@ -20,12 +20,15 @@ def game_body():
         elif random_number % i == 0:
             right_ans = 1
             i = random_number
-    mirror_ans = right_ans
     answer = prompt.string('Your answer: ')
+    return(answer, right_ans)
+
+
+def game_body_2(answer):
     if answer == 'yes':
         answer_for_comp = 0
         mirror_ans = 'no'
     elif answer == 'no':
         answer_for_comp = 1
         mirror_ans = 'yes'
-    return(answer, right_ans, answer_for_comp, mirror_ans)
+    return(answer_for_comp, mirror_ans)
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -3,7 +3,8 @@
 from brain_games.games.brain_games_logic import welcome
 from brain_games.games.brain_games_logic import answer_check, rounds_check
 from brain_games.games.brain_games_logic import ROUNDS_MAX_NUMBER
-from brain_games.games.brain_prime_logic import game_body, game_condition
+from brain_games.games.brain_prime_logic import game_condition
+from brain_games.games.brain_prime_logic import game_body_1, game_body_2
 
 
 def main():
@@ -11,7 +12,8 @@ def main():
     game_condition()
     i = 0
     while i <= (ROUNDS_MAX_NUMBER - 1):
-        (answer, right_ans, answer_for_comp, mirror_ans) = game_body()
+        (answer, right_ans) = game_body_1()
+        (answer_for_comp, mirror_ans) = game_body_2(answer)
         i = answer_check(answer, right_ans, answer_for_comp, mirror_ans, i)
     rounds_check(i, name)
 
